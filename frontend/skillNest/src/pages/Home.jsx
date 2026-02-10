5d1b182bbf2b0cb0f1489afc094288d5e4ae637b
Shashikant Yadav <yshashikant231217@gmail.com>
2026-02-10 13:19:15 +0530

    Update Home.jsx

--- frontend/skillNest/src/pages/Home.jsx
+++ frontend/skillNest/src/pages/Home.jsx
@@ -2,61 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="p-10">
+    <div className="text-center mt-20">
+      <h1 className="text-4xl font-bold">Welcome to SkillNest</h1>
+      <p className="mt-4 text-gray-600">
+        Discover short courses and workshops easily.
+      </p>
 
-      {/* HERO SECTION */}
-      <div className="bg-gray-200 p-10 rounded-md">
-        <p className="text-gray-700 text-xl mb-2">hero section</p>
-
-        <h1 className="text-4xl font-bold mb-4">
-          Discover short courses & workshop
-        </h1>
-
-        <p className="text-gray-500 text-2xl mb-6">
-          Learn web Dev , UI/UX , Marketing , and more
-        </p>
-
-        <Link
-          to="/courses"
-          className="text-2xl font-bold underline"
-        >
-          Explore Courses
-        </Link>
-      </div>
-
-      {/* WHY SKILLNEST */}
-      <h2 className="text-5xl font-bold mt-14 mb-10">Why SkillNest ?</h2>
-
-      <div className="grid grid-cols-3 gap-10">
-        <div className="bg-gray-200 p-8 text-2xl font-bold">
-          Expert mentors
-        </div>
-
-        <div className="bg-gray-200 p-8 text-2xl font-bold">
-          flexibale learning
-        </div>
-
-        <div className="bg-gray-200 p-8 text-2xl font-bold">
-          Affordable price
-        </div>
-      </div>
-
-      {/* POPULAR COURSES */}
-      <h2 className="text-5xl font-bold mt-16 mb-10">Popular courses</h2>
-
-      <div className="grid grid-cols-3 gap-10">
-        <div className="bg-gray-200 p-10 text-3xl font-bold">
-          Web Development
-        </div>
-
-        <div className="bg-gray-200 p-10 text-3xl font-bold">
-          UI/UX Design
-        </div>
-
-        <div className="bg-gray-200 p-10 text-3xl font-bold">
-          Digital Marketing
-        </div>
-      </div>
+      <Link
+        to="/courses"
+        className="inline-block mt-6 bg-orange-600 text-white px-6 py-2 rounded"
+      >
+        Explore Courses
+      </Link>
     </div>
   );
 }
